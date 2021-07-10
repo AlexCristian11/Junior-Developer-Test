ff247b4718dda12382434d8816d6ce803d1ec25a
AlexCristian11 <alexpopescu2002@yahoo.com>
2021-07-10 15:17:14 +0300

    Added index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product List</title>
+    <link rel="stylesheet" href="./styles/style-index.css">
+</head>
+
+<body>
+
+    <!-- HEADER -->
+
+    <header>
+        <h1>Product List</h1>
+        <div class="buttons">
+            <a href="./product-page.php"><button>ADD</button></a>
+            <button id="delete-product-btn">MASS DELETE</button>
+        </div>
+    </header>
+    <div id="line"></div>
+
+    <p id="show"></p>
+
+    <!-- FOOTER -->
+
+    <footer>
+        <h4>Scandiweb Test assignment</h4>
+    </footer>
+
+
+</body>
+<script src="./index.js"></script>
+
+</html>
